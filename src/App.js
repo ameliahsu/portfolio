6dbb8e28158cf9531cd2e0cb75a46a6e34e84643
Mia Hsu <ameliahsu@gmail.com>
2022-12-31 17:37:24 -0500

    add font and style name + bg

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
+import GridLines from 'react-gridlines'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <GridLines className="grid-area" lineColor={"#CCDAAF"} cellWidth={60} strokeWidth={6}>
+      <header className="name">
+        <h1>
+          mia hsu
+        </h1>
       </header>
+      </GridLines>
     </div>
   );
 }
