edf0c128b33fb0944bbfd2c3a2ae038064ffc728
Mia Hsu <ameliahsu@gmail.com>
2023-01-01 16:18:58 -0500

    i hate curves

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,38 @@
 import "./Styles.css";
 import Socials from "./components/Socials.js";
+import PageButton from "./components/PageButton";
+import ReactCurvedText from "react-curved-text";
 
 function Home() {
   return (
     <div className="home">
-      <header className="name">
+      <header className="name font">
         <h1>mia hsu</h1>
       </header>
       <Socials />
+      <ReactCurvedText
+        className="font"
+        width={300}
+        height={300}
+        cx="150"
+        cy="150"
+        rx={100}
+        ry={100}
+        startOffset={90}
+        reversed={true}
+        text="about me"
+        textProps={{
+          style: {
+            fontSize: "5vmin",
+            fontFamily: '"League Spartan", sans-serif',
+          },
+        }}
+        textPathProps={{
+          style: {
+            fill: "white",
+          },
+        }}
+      />
     </div>
   );
 }
---
+++ src/components/PageButton.js
@@ -0,0 +1,50 @@
+// import "../Styles.css";
+// import React, { Component } from "react";
+// import { View, StyleSheet } from "react-native";
+// import Svg, { G, Text, TextPath, TSpan, Circle } from "react-native-svg";
+
+// function PageButton() {
+//   return (
+//     <View
+//       style={{
+//         flex: 1,
+//         justifyContent: "center",
+//         paddingTop: Constants.statusBarHeight,
+//         padding: 0,
+//       }}
+//     >
+//       <Svg height="100%" width="100%" viewBox="0 0 300 300" {...props}>
+//         <G id="circle">
+//           <Circle
+//             r={100}
+//             x={150}
+//             y={150}
+//             fill="none"
+//             stroke="none"
+//             strokeWidth={0}
+//             transform="rotate(-135)"
+//           />
+//         </G>
+//         <Image
+//           style={{
+//             width: 220,
+//             height: 220,
+//             borderRadius: 110,
+//             marginLeft: 68,
+//             marginTop: 175,
+//           }}
+//           source={require("./doggy.jpg")}
+//         />
+//         <Text fill="#000" fontSize="14">
+//           <Text fill="#000" fontSize="14">
+//             <TextPath href="#circle">
+//               <TSpan dy={0}>Text along a curved path...</TSpan>
+//             </TextPath>
+//           </Text>
+//         </Text>
+//       </Svg>
+//     </View>
+//   );
+// }
+
+// export default PageButton;
