1798a95ee01bb1422d1d2a6c56c950c8dbc84496
Mia Hsu <ameliahsu@gmail.com>
2023-01-01 11:47:14 -0500

    refactor

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import "./App.css";
 import GridLines from "react-gridlines";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons"
-import IconButton from "./IconButton";
+import Home from "./Home.js";
 
 function App() {
   return (
@@ -14,10 +11,7 @@ function App() {
         cellWidth={60}
         strokeWidth={6}
       >
-        <header className="name">
-          <h1>mia hsu</h1>
-          <IconButton icon={<FontAwesomeIcon icon={faEnvelope} />}/>
-        </header>
+        <Home />
       </GridLines>
     </div>
   );
---
+++ src/Home.js
@@ -0,0 +1,21 @@
+import "./Home.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import IconButton from "./IconButton";
+
+function Home() {
+  return (
+    <div className="home">
+      <header className="name">
+        <h1>mia hsu</h1>
+      </header>
+      <IconButton
+        icon={<FontAwesomeIcon icon={faEnvelope} />}
+        link="https://google.com"
+      />
+    </div>
+  );
+}
+
+export default Home;
