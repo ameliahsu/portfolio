4b29baa277f9469d856904dc4539ac1e091cbfba
Mia Hsu <ameliahsu@gmail.com>
2023-01-01 21:07:08 -0500

    stick socials to bottom (footer)

--- src/Home.js
+++ src/Home.js
@@ -2,46 +2,47 @@ import "./Styles.css";
 import Socials from "./components/Socials.js";
 import PageButton from "./components/PageButton";
 
-// function Home() {
-//   return (
-//     <div className="home">
-//       <div className="bottom-row">
-//         <PageButton
-//           header={"graphic design"}
-//           image={"/img/cutedog.jfif"}
-//           offset={35}
-//         />
-//         <div className="center">
-//           <header className="name font">
-//             <h1>mia hsu</h1>
-//           </header>
-//           <Socials />
-//         </div>
-//         <PageButton
-//           header={"photography"}
-//           image={"/img/street.jpg"}
-//           offset={180}
-//         />
-//       </div>
-//     </div>
-//   );
-// }
-
 function Home() {
   return (
     <div className="home">
-      {/* <PageButton
+      <div className="bottom-row">
+        <header className="name font">
+          <h1>mia hsu</h1>
+        </header>
+        <div className="bottom-pages">
+          <PageButton
             header={"graphic design"}
             image={"/img/cutedog.jfif"}
-            offset={35}
+            offset={0}
           />
-      <header className="name font">
-        <h1>mia hsu</h1>
-      </header>
-      <Socials /> */}
-      <PageButton header={"about"} image={"/img/street.jpg"} offset={180} />
+          <div className="empty" />
+          <PageButton
+            header={"photography"}
+            image={"/img/street.jpg"}
+            offset={90}
+          />
+        </div>
+      </div>
+      <Socials />
     </div>
   );
 }
+
+// function Home() {
+//   return (
+//     <div className="home">
+//       {/* <PageButton
+//             header={"graphic design"}
+//             image={"/img/cutedog.jfif"}
+//             offset={35}
+//           />
+//       <header className="name font">
+//         <h1>mia hsu</h1>
+//       </header>
+//       <Socials /> */}
+//       <PageButton header={"about"} image={"/img/street.jpg"} offset={180} />
+//     </div>
+//   );
+// }
 
 export default Home;
