25035db7166a2443171f298be8eb33844f61698a
Mia Hsu <ameliahsu@gmail.com>
2023-01-02 10:56:42 -0500

    added top row btns before i break everything :D

--- src/Home.js
+++ src/Home.js
@@ -5,46 +5,47 @@ import PageButton from "./components/PageButton";
 function Home() {
   return (
     <div className="home">
-      <div className="bottom-row">
-        <header className="name font">
-          <h1 className="h1">mia hsu</h1>
-        </header>
-        <div className="bottom-pages">
+      <div className="top-wrapper">
+        <div className="top-row">
           <PageButton
-            header={"graphic design"}
-            image={"/img/cutedog.jfif"}
+            header={"about me"}
+            image={"/img/garden.jfif"}
             link={"https://ameliahsu.myportfolio.com/graphic-design"}
             offset={0}
           />
-          <div className="empty" />
           <PageButton
-            header={"photography"}
-            image={"/img/street.jpg"}
+            header={"robotics"}
+            image={"/img/howls.jpg"}
             link={"https://ameliahsu.myportfolio.com/digital-photos"}
             offset={90}
           />
+        </div>
+      </div>
+      <div className="bottom-wrapper">
+        <div className="bottom-row">
+          <header className="name font">
+            <h1 className="h1">mia hsu</h1>
+          </header>
+          <div className="bottom-pages">
+            <PageButton
+              header={"graphic design"}
+              image={"/img/cutedog.jfif"}
+              link={"https://ameliahsu.myportfolio.com/graphic-design"}
+              offset={0}
+            />
+            <div className="empty" />
+            <PageButton
+              header={"photography"}
+              image={"/img/street.jpg"}
+              link={"https://ameliahsu.myportfolio.com/digital-photos"}
+              offset={90}
+            />
+          </div>
         </div>
       </div>
       <Socials />
     </div>
   );
 }
-
-// function Home() {
-//   return (
-//     <div className="home">
-//       {/* <PageButton
-//             header={"graphic design"}
-//             image={"/img/cutedog.jfif"}
-//             offset={35}
-//           />
-//       <header className="name font">
-//         <h1>mia hsu</h1>
-//       </header>
-//       <Socials /> */}
-//       <PageButton header={"about"} image={"/img/street.jpg"} offset={180} />
-//     </div>
-//   );
-// }
 
 export default Home;
