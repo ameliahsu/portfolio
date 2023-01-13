e24e8911dae59b55ec3ade00ed65f070b1ee22b8
Mia Hsu <ameliahsu@gmail.com>
2023-01-13 18:09:16 -0500

    animate page pop-ups

--- src/components/PageContainer.js
+++ src/components/PageContainer.js
@@ -1,10 +1,16 @@
 import "../styles/PageContainer.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import { motion } from "framer-motion";
 
 function PageContainer({ title, contents, setPage }) {
   return (
-    <div className="container">
+    <motion.div
+      className="container"
+      initial={{ scale: 0 }}
+      animate={{ scale: 1 }}
+      exit={{ scale: 0 }}
+    >
       <div className="heading">
         <h1 className="title">{title}</h1>
         <button
@@ -17,7 +23,7 @@ function PageContainer({ title, contents, setPage }) {
         </button>
       </div>
       <div className="contents">{contents}</div>
-    </div>
+    </motion.div>
   );
 }
 
