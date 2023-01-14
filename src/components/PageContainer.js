8d46afd18b4cc44c139669d3b9b45e90b69ebded
Mia Hsu <ameliahsu@gmail.com>
2023-01-14 14:34:49 -0500

    SMOOTH ANIMATIONS WOOHOO

--- src/components/PageContainer.js
+++ src/components/PageContainer.js
@@ -1,15 +1,19 @@
+import React from "react";
 import "../styles/PageContainer.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 import { motion } from "framer-motion";
 
 function PageContainer({ title, contents, setPage }) {
+  const [open, setOpen] = React.useState(false);
   return (
     <motion.div
       className="container"
       initial={{ scale: 0 }}
       animate={{ scale: 1 }}
       exit={{ scale: 0 }}
+      onAnimationStart={() => setOpen(false)}
+      onAnimationComplete={() => setOpen(true)}
     >
       <div className="heading">
         <h1 className="title">{title}</h1>
@@ -22,7 +26,7 @@ function PageContainer({ title, contents, setPage }) {
           {<FontAwesomeIcon icon={faXmark} />}
         </button>
       </div>
-      <div className="contents">{contents}</div>
+      <div className="contents">{open && contents}</div>
     </motion.div>
   );
 }
