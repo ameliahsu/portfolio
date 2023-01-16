94ca0f045da7e83b1ab773fb0b530b0c55a3a527
Mia Hsu <ameliahsu@gmail.com>
2023-01-16 17:54:38 -0500

    update readme

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -9,7 +9,7 @@ function AboutContents() {
     <div className="aboutWrapper">
       <img className="profile" src="./img/MIA.jpg" alt="Mia Hsu" />
       <p>
-        Hi, my name is <strong>Mia Hsu</strong>!
+        Hi, my name is <strong>Mia Hsu</strong> (she/her)!
         <br />I am a <strong>computer science</strong> student at the University
         of Maryland College Park and a part of the Advanced Cybersecurity
         Experience for Students (<a href="https://aces.umd.edu/">ACES</a>)
@@ -20,19 +20,20 @@ function AboutContents() {
         <br />
         In my free time, I enjoy expressing my creativity through{" "}
         <strong>graphic design</strong> and <strong>photography</strong>, as
-        well as crocheting and scrapbooking occasionally. I also love
-        discovering new{" "}
+        well as crocheting and scrapbooking occasionally. I love discovering new{" "}
         <a href="https://open.spotify.com/user/1219146661?si=8a6afc6c9a964706">
           music
         </a>{" "}
         when I get the chance- my current favorite genres are K-Pop, K-R&B, and
-        K-Rap!
+        K-Rap! I am also a plant mom, foodie, and bread lover ʕ•ᴥ•ʔ.
         <br />
         I'm always open to new opportunities and always looking for ways to grow
         and learn. If you have any advice or resources to share, or just want to
         chat, don't hesitate to{" "}
         <a href="mailto:ameliahsu@gmail.com">reach out</a>
         ! I would love to hear from you.
+        <br />
+        Thanks for checking out my site!
         <br />
       </p>
       <ExternalButton
